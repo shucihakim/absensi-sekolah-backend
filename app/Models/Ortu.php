74652de02fc0e85766fdb7287e5ed83bfc62b70f
MRHRTZ <hanifsyauqi61@gmail.com>
2024-05-28 01:07:10 +0700

    bikin model

---
+++ app/Models/Ortu.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ortu extends Model
+{
+    use HasFactory;
+    
+    protected $table = 'ortu';
+    protected $guarded = [];
+}
